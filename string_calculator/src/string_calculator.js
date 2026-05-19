f19bfa17e07ac5837ab7c36c9af9353da687dc1d
Martina Fischer <if25b118@technikum-wien.at>
2026-05-19 11:29:25 +0200

    TC_5 OK and refactoring

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -2,12 +2,16 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	let sum = 0;
-    for (let i = 0; i < string_numbers.length; i++) {
-        if (string_numbers[i] == 3) {
-            sum += 2;
-        } else if (string_numbers[i] == 5) {
-            sum += 4;
-        }
-    }
-    return sum;
+    if (string_numbers.length >= 4 && string_numbers.length <= 6) {
+		for (let i = 0; i < string_numbers.length; i++) {
+			if (string_numbers[i] == 3) {
+				sum += 2;
+			} else if (string_numbers[i] == 5) {
+				sum += 4;
+			}
+			return sum;
+		}
+    } else {
+		return "Invalid input";
+	} 
 };
